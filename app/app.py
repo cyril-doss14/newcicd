49d4f8a78d16a0601f1e81a2fcccecadae72dec3
cyril-doss14 <cyrildoss007@gmail.com>
2024-06-23 12:57:11 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD Jenkins Application for Demo Purpose Do14WD batch'
+    return 'Trying to work with jenkins pipeline and email notification!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
