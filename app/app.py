3db9b49276f17fae5e39b9cbcbad928307f4d108
cyril-doss14 <cyrildoss007@gmail.com>
2024-06-23 13:18:54 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Trying to work with jenkins pipeline and email notification!'
+    return 'Upadte: Working with jenkins pipeline and email notification successful!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
